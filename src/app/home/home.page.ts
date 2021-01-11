d3cda7b04097ab398d744cba9630a3a40fc04601
rifkihariyanto25 <hariyantorifki25@gmail.com>
2021-01-11 16:07:12 +0700

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
-@Component({
+import { NavController } from '@ionic/angular';
+  
+  @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+  first;
+  second;
+  result;
+
+  constructor(public navcontrol : NavController) {}
+
+  addition(){
+    this.result = parseInt(this.first) + parseInt(this.second);
+  }
+
+  subraction(){
+    this.result = parseInt(this.first) - parseInt(this.second);
+  }
+
+  multiplied(){
+    this.result = parseInt(this.first) * parseInt(this.second);
+  }
+
+  division(){
+    this.result = parseInt(this.first) / parseInt(this.second);
+  }
 
 }
